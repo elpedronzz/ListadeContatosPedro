4a2b40f6dd5948d06adb335c50a6de9393247b01
elpedronzz <pedrojosepaesdebarros@outlook.com>
2024-04-23 16:39:18 -0300

    criado o contato.cs
    Criado o contato.cs com os métodos construtores da classe e suas propriedades.

--- ListadeContatosPedro/contato.cs
+++ ListadeContatosPedro/contato.cs
@@ -8,5 +8,52 @@ namespace ListadeContatosPedro
 {
     internal class contato
     {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        public string Nome
+        {
+            get 
+            {
+                return nome; 
+            } set 
+            { nome = value; 
+            } 
+        }
+        public string Sobrenome
+        { 
+            get 
+            { 
+                return sobrenome; 
+            } set { 
+                sobrenome = value; 
+            } 
+        }
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(00) 00000-0000";
+                }
+            }
+        }
+
+        public contato()
+        {
+            Nome = "Hideki"
+            Sobrenome = "Yang III"
+            Telefone = "(11) 91823-1831"
+        }
     }
 }
